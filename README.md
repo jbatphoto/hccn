036002a1b693b2f0ad8f56a003224370ffc41066
jbatphoto <justin.battenfield@lifechurch.tv>
2016-10-12 15:04:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Craft CMS Local install and Gulp workflow
+
+Requires [Nodejs](http://nodejs.org/) and [Gulp](http://gulpjs.com/)
+
+## Features
+
+- Installs latest version of [Craft CMS](http://buildwithcraft.com)
+- Installs Zurb Foundation 6 for sites
+- Installs Multi-environment config
+- Sass compilation and prefixing
+- Built-in BrowserSync server
+- For production builds:
+  - CSS compression
+  - JavaScript compression
+  - Image compression
+  - UnCSS
+  - Browser prefixing
+
+- - -
+
+
+
+
+Manual Setup
+-
+
+To set up a Craft CMS site, first download it with Git:
+
+```bash
+git clone https://github.com/johanneslamers/craft-workflow projectname
+```
+
+Then open the folder in your command line, and install the needed dependencies:
+
+```bash
+cd projectname
+bash install.sh
+```
+
+```bash
+npm install
+bower install
+```
+
+Finally, run **`npm start`** to run Gulp.
+Your finished site will be created in a folder called `public` , viewable at this URL:
+
+```
+http://localhost:8000
+```
+
+To create compressed, production-ready assets, run
+
+**`npm run build`**
+
+
+Tasks
+-
+
+
+* ```gulp watch``` to start the watch task, at the moment you will have to use a browser extention for live reload.
+
+* ```gulp``` to build for production, all the magic happens and template files will be moved to ```craft/templates```, resources (images/js/css) will be concatinated, minified and wrapped in silk before they end up in ```/public```.
+
+* ```gulp bower``` Injects bower dependencies into ```_layout.html```. This task will run on ```gulp watch``` as well.
+_Remember to ```--save``` when installing components_.
+
+
+TODO
+-
+
+- Add clean function to gulpfile.js
+- Add gulp useref
+- Remove gulp 
+# hccn
---
+++ gulpfile.js
@@ -0,0 +1,227 @@
+var $           = require('gulp-load-plugins')();
+var argv        = require('yargs').argv;
+var browser     = require('browser-sync');
+var gulp        = require('gulp');
+var rimraf      = require('rimraf');
+var sequence    = require('run-sequence');
+var sherpa      = require('style-sherpa');
+var gulp-if     = require('gulp-if'),
+var useref      = require('gulp-useref');
+var wierdep     = require('wiredep').stream;
+
+// Check for --production flag
+var isProduction = !!(argv.production);
+
+// Port to use for the development server.
+var PORT = 8000;
+
+// Browsers to target when prefixing CSS.
+var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
+
+// File paths to various assets are defined here.
+var PATHS = {
+    assets: [
+        'src/assets/**/*',
+        '!src/assets/{!img,js,scss}/**/*'
+    ],
+    sass: [
+        'bower_components/foundation-sites/scss',
+        'bower_components/motion-ui/src/'
+    ],
+
+    // All JavaScript files in the src/assets/js folder, along with Foundation and its dependencies, are bundled into one file called app.js. The files are bundled in this order:
+
+    javascript: [
+        'bower_components/what-input/what-input.js',
+        'src/assets/js/**/*.js',
+        'src/assets/js/app.js'
+    ],
+
+    javascript_foundation: [
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/foundation-sites/js/foundation.core.js',
+        'bower_components/foundation-sites/js/foundation.util.*.js',
+        'bower_components/foundation-sites/js/foundation.*.js',
+
+    ]
+};
+
+// Delete the "dist" folder
+// This happens every time a build starts
+// gulp.task('clean', function(done) {
+//   rimraf('./dist', done);
+// });
+
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+gulp.task('copy', function() {
+    gulp.src(PATHS.assets)
+    .pipe(gulp.dest('./public/assets'));
+});
+
+
+// gulp.task('pages', function() {
+//   // gulp.src('./src/pages/**/*.{html,hbs,handlebars}')
+//   //   .pipe(panini({
+//   //     root: './src/pages/',
+//   //     layouts: './src/layouts/',
+//   //     partials: './src/partials/',
+//   //     data: './src/data/',
+//   //     helpers: './src/helpers/'
+//   //   }))
+//   //   .pipe(gulp.dest('./dist'));
+// });
+
+// gulp.task('pages:reset', function(cb) {
+//     // panini.refresh();
+//     gulp.run('pages');
+//     browser.reload();
+// });
+
+// gulp.task('styleguide', function(cb) {
+//   sherpa('./src/styleguide/index.md', {
+//     output: './dist/styleguide.html',
+//     template: './src/styleguide/template.html'
+//   }, cb);
+// });
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+gulp.task('sass', function() {
+    var uncss = $.if(isProduction, $.uncss({
+        html: ['./templates/*.twig', './templates/**/*.twig', './templates/*.html', './templates/**/*.html'],
+        ignore: [
+            new RegExp('^meta\..*'),
+            new RegExp('^\.is-.*')
+        ]
+    }));
+
+    var minifycss = $.if(isProduction, $.minifyCss());
+
+    return gulp.src('./src/assets/scss/app.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+        includePaths: PATHS.sass
+    })
+    .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+        browsers: COMPATIBILITY
+    }))
+
+
+    // uncomment this if you want to uncss
+    // .pipe(uncss)
+    .pipe(minifycss)
+    .pipe($.if(!isProduction, $.sourcemaps.write()))
+    .pipe(gulp.dest('./public/assets/css'));
+});
+
+
+// Combine JavaScript into one file
+// In production, the file is minified
+gulp.task('javascript_foundation', function() {
+    var uglify = $.if(isProduction, $.uglify()
+    .on('error', function (e) {
+        console.log(e);
+    }));
+
+    return gulp.src(PATHS.javascript_foundation)
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('foundation.js'))
+    .pipe(uglify)
+    .pipe($.if(!isProduction, $.sourcemaps.write()))
+    .pipe(gulp.dest('./public/assets/js'));
+});
+
+// Combine JavaScript into one file
+// In production, the file is minified
+gulp.task('javascript', function() {
+    var uglify = $.if(isProduction, $.uglify()
+    .on('error', function (e) {
+        console.log(e);
+    }));
+
+    return gulp.src(PATHS.javascript)
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('app.js'))
+    .pipe(uglify)
+    .pipe($.if(!isProduction, $.sourcemaps.write()))
+    .pipe(gulp.dest('./public/assets/js'));
+});
+
+// Copy images to the "dist" folder
+// In production, the images are compressed
+gulp.task('images', function() {
+    var imagemin = $.imagemin({
+        progressive: true,
+        interlaced: true
+    });
+
+    return gulp.src('./src/assets/img/**/*')
+    .pipe(imagemin)
+    .pipe(gulp.dest('./public/assets/img'));
+});
+
+
+gulp.task('fonts', ['move'], function () {
+    return gulp.src('./src/assets/fonts/**/*')
+    .pipe(gulp.dest('./public/assets/fonts'));
+});
+
+
+gulp.task('wiredep', function() {
+    return gulp.src('/templates/_layout/_layout.twig')
+    .pipe(wiredep({
+        exclude: [foundation-sites],
+        exclude: [modernizr]
+    }))
+    .pipe(gulp.dest('templates/_layout/'));
+});
+
+// gulp.task('useref', function () {
+//   gulp.src('./templates/_layout/_layout.twig')
+//     .pipe(useref({
+//
+//     }))
+//     .pipe(gulp.dest('./templates/_layout/'));
+// });
+
+
+
+// Build the "dist" folder by running all of the above tasks
+gulp.task('build', function(done) {
+    // sequence('clean', ['pages', 'sass', 'javascript', 'images', 'copy'], 'styleguide', done);
+    sequence(['sass', 'javascript', 'javascript_foundation', 'images', 'fonts', 'copy' ],  done);
+});
+
+// Start a server with LiveReload to preview the site in
+gulp.task('server', ['build'], function() {
+    browser.init({
+        server: './public', port: PORT
+    });
+});
+
+
+
+
+
+
+
+// gulp.task('bower', function () {
+//   gulp.src('./app/templates/_layout.twig')
+//     .pipe(wiredep({
+//
+//     }))
+//     .pipe(gulp.dest('./app/templates/'));
+// });
+
+// Build the site, run the server, and watch for file changes
+gulp.task('default', ['build', 'server'], function() {
+    gulp.watch(PATHS.assets, ['copy', browser.reload]);
+    gulp.watch(['./templates/*.twig'], [browser.reload]);
+    gulp.watch(['./templates/**/*.twig'], [browser.reload]);
+    gulp.watch(['./src/assets/scss/**/*.scss'], ['sass', browser.reload]);
+    gulp.watch(['./src/assets/js/**/*.js'], ['javascript', browser.reload]);
+    gulp.watch(['./src/assets/img/**/*'], ['images', browser.reload]);
+    gulp.watch(['./src/styleguide/**'], ['styleguide', browser.reload]);
+});
